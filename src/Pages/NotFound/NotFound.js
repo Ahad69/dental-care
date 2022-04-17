d4b0699a159d2bbaa000aaabdbfac7246cc0d013
Ahad hossain <ahadxx99@gmail.com>
2022-04-17 08:48:39 -0500

    Readme file modified

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            not
+        <div className='notfound'>
+            
+           <img  src="https://miro.medium.com/max/1400/1*cLQUX8jM2bMdwMcV2yXWYA.jpeg" alt="" />
         </div>
     );
 };
